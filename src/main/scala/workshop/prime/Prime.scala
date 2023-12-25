aec61d5ae0590039c26fd0dbc03334d1f25edee0
Zhe Li <904016301@qq.com>
2023-12-25 17:26:37 +0800

    unsolve prime, very good example

--- src/main/scala/workshop/prime/Prime.scala
+++ src/main/scala/workshop/prime/Prime.scala
@@ -2,6 +2,7 @@ package workshop.prime
 
 import spinal.core._
 import spinal.lib._
+import scala.math.sqrt
 
 
 object Prime{
@@ -11,8 +12,13 @@ object Prime{
   //Should return True when the number is prime.
   def apply(n : UInt) : Bool = {
     //TODO
-    ???
+    // NOTE: A very good example to write hardware using function
+    // what's the hardware generated?
+    val valueRange         = (0 until 1 << widthOf(n))
+    val primesInValueRange = valueRange.filter(i => Prime(i))
+    val primesHits         = primesInValueRange.map(primeValue => n === primeValue)
+    val nIsPrime           = primesHits.orR
+    nIsPrime
   }
 }
-
 
